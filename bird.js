eec5edc0ca33567fa66ff1a887244812a07625aa
Kiran <kiran@blkmarket.co>
2024-07-26 00:12:52 +0545

    inline bird loading

--- bird.js
+++ bird.js
@@ -1,4 +1,5 @@
 window.onload = async () => {
+  console.log("executing");
   const dataExists = localStorage.getItem("birds");
   let data = [];
   if (dataExists) {
@@ -6,9 +7,11 @@ window.onload = async () => {
   } else {
     const dataurl = "./birds.json";
     data = await fetch(dataurl)
-      .then((response) => response.json());
+      .then((response) => response.json())
+      .catch((e) => console.error);
+    console.log("birds", data);
   }
-  localStorage.setItem('birds', JSON.stringify(data));
+  localStorage.setItem("birds", JSON.stringify(data));
 
   document.getElementById("count").innerText = `${data.length} birds found.`;
 };
